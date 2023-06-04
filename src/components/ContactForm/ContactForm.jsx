55ef5e82b7d762f955e254f31bd253601a2f0776
Serhii <116863810+Serhii-K5@users.noreply.github.com>
2023-06-04 22:14:40 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -25,41 +25,38 @@ export const ContactForm = ({handleSubmit}) => {
     // form.reset();
   };
 
-  // render() {
-  //   const { name, number } = this.state;
-
-    return (
-      <form className={css.form} onSubmit={handleFormSubmit}>
-        <label className={css.formLabel}>Name </label>
-        <input
-          className={css.formName}
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required          
-          onChange={handleChangeName}          
-          value={name}
-          placeholder="Enter name"
-        />
-        <label className={css.formLabel}>Number </label>
-        <input
-          className={css.formNumber}
-          type="tel"
-          name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-          onChange={handleChangeNumber}
-          value={number}
-          placeholder="Enter phone number"
-        />
-        <button className={css.formBtn} type="submit">
-          Add contact
-        </button>
-      </form>
-    );
-  }
+  return (
+    <form className={css.form} onSubmit={handleFormSubmit}>
+      <label className={css.formLabel}>Name </label>
+      <input
+        className={css.formName}
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required          
+        onChange={handleChangeName}          
+        value={name}
+        placeholder="Enter name"
+      />
+      <label className={css.formLabel}>Number </label>
+      <input
+        className={css.formNumber}
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+        onChange={handleChangeNumber}
+        value={number}
+        placeholder="Enter phone number"
+      />
+      <button className={css.formBtn} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+}
 
 ContactForm.propTypes = {
   handleSubmit: propTypes.func.isRequired,
